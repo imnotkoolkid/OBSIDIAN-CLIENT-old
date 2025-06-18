a0e39b4143e310b8e5de45926be244753e1042ec
imnotkoolkid <imnotkoolkid@gmail.com>
2025-06-18 22:00:09 +0530

    Update scriptsPreload.js

--- scriptsPreload.js
+++ scriptsPreload.js
@@ -3,27 +3,36 @@ const path = require('path');
 const fs = require('fs');
 
 const scriptsPath = ipcRenderer.sendSync('get-scripts-path');
+const userData = ipcRenderer.sendSync('get-user-data-path');
+const settingsPath = path.join(userData, '../ObsidianClient/clientsettings/settings.dat');
 const loadedScripts = [];
 
+let disabledScripts = [];
 try {
-  if (fs.existsSync(scriptsPath)) {
-    fs.readdirSync(scriptsPath)
-      .filter(file => file.endsWith('.js'))
-      .forEach(file => {
-        try {
-          require(path.join(scriptsPath, file));
-          loadedScripts.push(file);
-          console.log(`Loaded script: ${file}`);
-        } catch (error) {
-          console.error(`Error loading script ${file}:`, error);
-        }
-      });
-  } else {
-    console.warn('Scripts folder does not exist:', scriptsPath);
+  if (fs.existsSync(settingsPath)) {
+    const settings = JSON.parse(fs.readFileSync(settingsPath, 'utf8'));
+    disabledScripts = settings.disabledScripts || [];
   }
+} catch (error) {
+  console.error('Error reading settings:', error);
+  disabledScripts = [];
+}
+
+try {
+  const scriptFiles = fs.readdirSync(scriptsPath).filter(file => file.endsWith('.js'));
+
+  scriptFiles.forEach(file => {
+    if (!disabledScripts.includes(file)) {
+      try {
+        require(path.join(scriptsPath, file));
+        loadedScripts.push(file);
+      } catch (error) {
+        console.error(`Error loading script ${file}:`, error);
+      }
+    }
+  });
 } catch (error) {
   console.error('Error reading scripts folder:', error);
 }
 
 ipcRenderer.send('set-preloaded-scripts', loadedScripts);
-ipcRenderer.on('get-preloaded-scripts', e => e.returnValue = loadedScripts);
