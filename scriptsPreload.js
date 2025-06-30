b4bcfa1c1008d95903491338f238488ddd0b9f1a
imnotkoolkid <imnotkoolkid@gmail.com>
2025-06-30 21:09:34 +0530

    Update scriptsPreload.js

--- scriptsPreload.js
+++ scriptsPreload.js
@@ -1,24 +1,39 @@
-const { ipcRenderer } = require('electron');
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
 
-const scriptsPath = ipcRenderer.sendSync('get-scripts-path');
-const loadedScripts = [];
+class ScriptHandler {
+  constructor(scriptsPath) {
+    this.scriptsPath = scriptsPath;
+  }
 
-try {
-  const disabledScripts = ipcRenderer.sendSync('get-disabled-scripts') || [];
-  fs.readdirSync(scriptsPath).filter(f => f.endsWith('.js')).forEach(file => {
-    if (!disabledScripts.includes(file)) {
-      try {
-        require(path.join(scriptsPath, file));
-        loadedScripts.push(file);
-      } catch (err) {
-        console.error(`Error loading script ${file}:`, err);
-      }
+  getScriptsPath() {
+    return this.scriptsPath;
+  }
+
+  getAllScripts() {
+    try {
+      return fs.readdirSync(this.scriptsPath).filter(f => f.endsWith('.js'));
+    } catch (err) {
+      console.error('Error reading scripts folder:', err);
+      return [];
     }
-  });
-} catch (err) {
-  console.error('Error reading scripts folder:', err);
+  }
+
+  getDisabledScripts(settings) {
+    return settings.disabledScripts || [];
+  }
+
+  getNewDisabledScripts(settings, script, enabled) {
+    let disabledScripts = settings.disabledScripts || [];
+    if (enabled) {
+      return disabledScripts.filter(s => s !== script);
+    } else {
+      if (!disabledScripts.includes(script)) {
+        return [...disabledScripts, script];
+      }
+      return disabledScripts;
+    }
+  }
 }
 
-ipcRenderer.send('set-preloaded-scripts', loadedScripts);
+module.exports = ScriptHandler;
